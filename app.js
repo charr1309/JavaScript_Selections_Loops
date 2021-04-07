7af217a93e2b4a2e3f2daedb3bcd17dc608c44cf
charr1309 <ceh710@aol.com>
2021-04-07 10:47:11 -0400

    first commit

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+for(i=1;i<=100; i++){
+    if(i%2!==0){
+        console.log("this is an odd number")
+        }
+    }
+}
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+for(i=1;i<=100;i++){
+    if(i%3===0){
+        let res="fizz";
+    }else{
+        if(i%5 === 0);
+            let res2 = "buzz";
+            let final = res + res2
+    }
+    console.log(final);
+}
